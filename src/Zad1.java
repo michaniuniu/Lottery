0b7863eda01a0ea8b3b564e5fd0d16fbc385f91c
michaniuniu <michal.sikora@vp.pl>
2017-05-10 23:23:08 +0200

    First commit

---
+++ src/Zad1.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.lang.String;
+public class Zad1 {
+
+	public static void main(String[] args) {		
+		Random random = new Random();
+		int target,i,choice;		
+		String outOrNot = new String();
+		Scanner scan = new Scanner(System.in);
+		//Scanner scan1 = new Scanner(System.in);
+	do{
+		System.out.println("Masz za zadanie odgarn¹æ liczbê losow¹ wygenerowan¹ przez program.\nNa Twoje szczêœcie uzyskasz ma³e podpowiedzi.");
+		target = random.nextInt(100);System.out.println(target);	
+		choice = scan.nextInt();
+		i = 0;
+		do{
+			if (choice<target)
+				System.out.println("Musisz celowaæ wy¿ej");
+			if (choice>target)
+				System.out.println("Musisz celowaæ ni¿ej");
+			if (choice!=target)
+				choice = scan.nextInt();
+			i++;
+		}while(choice!=target);
+		
+		System.out.println("Brawo strza³ w dziesi¹tkê!");
+		System.out.println("Dokona³eœ "+i+" prób.");
+		System.out.println("Czy chesz zagrac jeszcze raz?");
+		scan.nextLine();
+		outOrNot = scan.nextLine();
+	
+	}while(outOrNot.equalsIgnoreCase("tak") || outOrNot.equalsIgnoreCase("t"));
+	scan.close();
+	}
+}
