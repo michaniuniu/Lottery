7f8e170777f868e4fe69dc6e420059ca3fa6e4e8
michaniuniu <michal.sikora@vp.pl>
2017-05-12 18:04:01 +0200

    Final

--- src/Zad1.java
+++ src/Zad1.java
@@ -9,24 +9,24 @@ public class Zad1 {
 		String outOrNot = new String();
 		Scanner scan = new Scanner(System.in);
 		do{
-			System.out.println("Masz za zadanie odgarnąć liczbę losowo wygenerowaną przez program.\n
-				   Na Twoje szczęście uzyskasz małe podpowiedzi.");
+			System.out.println("Masz za zadanie odgarnąć liczbę losowo wygenerowaną przez program.\n"
+					+ "Na Twoje szczęście uzyskasz małe podpowiedzi.");
 			target = random.nextInt(100);
 			//System.out.println(target);	
 			choice = scan.nextInt();
 			i = 0;
 			do{
 				if (choice<target)
-					System.out.println("Musisz celowaæ wy¿ej");
+					System.out.println("Musisz celować wyżej");
 				if (choice>target)
-					System.out.println("Musisz celowaæ ni¿ej");
+					System.out.println("Musisz celować niżej");
 				if (choice!=target)
 					choice = scan.nextInt();
 				i++;
 			}while(choice!=target);
 			
-			System.out.println("Brawo strza³ w dziesi¹tkê!");
-			System.out.println("Dokona³eœ "+i+" prób.");
+			System.out.println("Brawo strzał w dziesiątkę!");
+			System.out.println("Dokonałeś "+i+" prób.");
 			System.out.println("Czy chesz zagrac jeszcze raz?");
 			scan.nextLine();
 			outOrNot = scan.nextLine();
